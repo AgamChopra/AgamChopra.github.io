306f299f38239ee05971b10f1804cf61463009f6
AgamChopra <work.agamchopra@gmail.com>
2024-10-09 13:52:48 -0500

    initial code added

---
+++ script.js
@@ -0,0 +1,23 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tabbuttons;
+
+    // Hide all tab contents
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove active class from all buttons
+    tabbuttons = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabbuttons.length; i++) {
+        tabbuttons[i].className = tabbuttons[i].className.replace(" active", "");
+    }
+
+    // Show the current tab and add active class to the clicked button
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Set the default tab to open
+document.getElementById("defaultTab").click();
+
